fc5a6c57bf8a67650ce76c1c31b88fddd0f4b8f2
Louis Le Brun <louis.lebrun3@free.fr>
2018-07-17 07:07:00 +0200

    Updated public list

--- components/PublicList/PublicListStack.js
+++ components/PublicList/PublicListStack.js
@@ -20,11 +20,6 @@ export const PublicListStack = createStackNavigator({
                 color: 'white',
                 width: 100
             },
-            headerRight: (
-                <TouchableOpacity>
-                    <Image source={require('../../assets/images/add_item.png')} style={{width: 24, height: 24, marginRight:16}}/>
-                </TouchableOpacity>
-            ),
             title: 'Publique'
         })
     }
